f7e06b6ea67957ec747b214602110653b5663c18
mmarusza <66088741+mmarusza@users.noreply.github.com>
2020-07-10 16:19:28 -0400

    reasearch and testing
    getting things together

--- gui.py
+++ gui.py
@@ -1,28 +1,23 @@
 import tkinter as tk
     
 def pac():
-    import pacman.py
+    import pacc.py
 
-def minesweeper():
-    from nachomines.scripts.main import run
-    if __name__ == "__main__":
-        run()
+def tiktak():
+    import tiktak.py
+
+def snake():
+    import snake.py
 
 root = tk.Tk()
 frame = tk.Frame(root)
 frame.pack()
 
-button = tk.Button(frame, 
-                   text="QUIT", 
-                   fg="red",
-                   command=quit)
+button = tk.Button(frame, text="QUIT", fg="red", command=quit)
 button.pack(side=tk.LEFT)
-slogan = tk.Button(frame,
-                   text="Pac Man",
-                   command= pac)
+slogan = tk.Button(frame, text= "Snake", fg="yellow", width= 6, height= 3, command= snake) 
+slogan = tk.Button(frame, text="Tik Tak", fg="blue", command= tiktak)
+slogan = tk.Button(frame, text="Pac Man ", fg="green", command= pac)
 slogan.pack(side=tk.LEFT)
-slogan = tk.Button(frame,
-                   text="Minesweeper",
-                   command= minesweeper)
 slogan.pack(side=tk.RIGHT)
 root.mainloop()
