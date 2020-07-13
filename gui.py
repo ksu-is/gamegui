243fc844888515d0801dd6aa59851a227e9f4aad
mmarusza <66088741+mmarusza@users.noreply.github.com>
2020-07-13 18:00:26 -0400

    changes for sprint 2 adding more detail
    added a new game and cleaned up the code for the gui

--- gui.py
+++ gui.py
@@ -1,23 +1,26 @@
-import tkinter as tk
-    
-def pac():
-    import pacc.py
-
-def tiktak():
-    import tiktak.py
-
-def snake():
-    import snake.py
+import tkinter as tk 
 
 root = tk.Tk()
 frame = tk.Frame(root)
 frame.pack()
 
-button = tk.Button(frame, text="QUIT", fg="red", command=quit)
+
+def pac():
+    import(pacc.py)
+def tiktak():
+    import tiktak.py  
+def snake():
+    import snake.py  
+
+
+
+button = tk.Button(frame, text="QUIT", fg="red", bg= "purple", command=quit)
 button.pack(side=tk.LEFT)
-slogan = tk.Button(frame, text= "Snake", fg="yellow", width= 6, height= 3, command= snake) 
-slogan = tk.Button(frame, text="Tik Tak", fg="blue", command= tiktak)
-slogan = tk.Button(frame, text="Pac Man ", fg="green", command= pac)
-slogan.pack(side=tk.LEFT)
-slogan.pack(side=tk.RIGHT)
+button = tk.Button(frame, text= "Snake", fg="yellow", bg= "green", command= snake) 
+button.pack(side=tk.LEFT)
+button = tk.Button(frame, text="Tik Tak Toe", fg="blue",bg= "pink", command= tiktak)
+button.pack(side=tk.LEFT)
+button = tk.Button(frame, text="Pac Man", fg="green", bg= "blue", command= pac)
+button.pack(side=tk.LEFT)
+
 root.mainloop()
