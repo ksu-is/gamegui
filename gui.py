a311bbc5c6533c84efab10aae8ccca4903b1f9ea
mmarusza <66088741+mmarusza@users.noreply.github.com>
2020-07-13 20:30:22 -0400

    game search
    Added the webbrowser package and made a search button to search games not on gui to play online

--- gui.py
+++ gui.py
@@ -1,4 +1,6 @@
 import tkinter as tk 
+from tkinter import *
+import webbrowser
 
 root = tk.Tk()
 frame = tk.Frame(root)
@@ -6,21 +8,26 @@ frame.pack()
 
 
 def pac():
-    import(pacc.py)
+    import pacc.py
 def tiktak():
     import tiktak.py  
 def snake():
     import snake.py  
+def search():
+    webbrowser.open("https://www.miniclip.com/games/search/en/?query=" + e.get(), "#t-sd")
 
 
 
 button = tk.Button(frame, text="QUIT", fg="red", bg= "purple", command=quit)
-button.pack(side=tk.LEFT)
+button.pack(side=tk.RIGHT)
 button = tk.Button(frame, text= "Snake", fg="yellow", bg= "green", command= snake) 
 button.pack(side=tk.LEFT)
 button = tk.Button(frame, text="Tik Tak Toe", fg="blue",bg= "pink", command= tiktak)
 button.pack(side=tk.LEFT)
 button = tk.Button(frame, text="Pac Man", fg="green", bg= "blue", command= pac)
 button.pack(side=tk.LEFT)
-
+e = Entry(root)
+e.pack()
+button = Button(root, text= "Search for game", command= search)
+button.pack(side=tk.BOTTOM)
 root.mainloop()
